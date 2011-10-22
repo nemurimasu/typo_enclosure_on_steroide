3fc40a0b57e22522df0b061d0dbe1cb1106c6f04
Nemurimasu Neiro <nemurimasu@gmail.com>
2011-10-22 18:58:07 +0000

    add html5 audio

--- lib/typo_enclosure_on_steroide.rb
+++ lib/typo_enclosure_on_steroide.rb
@@ -5,10 +5,19 @@ end
 
 module TypoEnclosureOnSteroideHelper
   
-  def display_mp3(filename)
-    html = "<li><object type='application/x-shockwave-flash' data='/dewplayer.swf?mp3=/files/#{filename}' width='200' height='20'>"
+  def display_mp3(filename, has_ogg)
+    html = "<li><audio controls>"
+    html << "<source src=\"/files/#{filename}.mp3\" type=\"audio/mpeg\" />"
+    html << "<source src=\"/files/#{filename}.ogg\" type=\"audio/ogg\" />" if has_ogg
+    html << "<object type='application/x-shockwave-flash' data='/dewplayer.swf?mp3=/files/#{filename}' width='200' height='20'>"
     html << "<param name='movie' value='/dewplayer.swf?mp3=/files/#{filename}' />"
-    html << "</object></li>"
+    html << "</object></audio></li>"
+  end
+
+  def display_ogg(filename)
+    html = "<li><audio controls>"
+    html << "<source src=\"/files/#{filename}.ogg\" type=\"audio/ogg\" />"
+    html << "</audio></li>"
   end
   
   def display_simple_file(filename)
@@ -23,12 +32,24 @@ module TypoEnclosureOnSteroideHelper
     html = "<h3>Attachments</h3>"
     html << "<ul>"
     
+    mp3s = []
+    oggs = []
+    others = []
     article.resources.each do |resource|
-      if resource.filename =~ /.mp3/
-        html << display_mp3(resource.filename)
-      else
-        html << display_simple_file(resource.filename)
+      case File.extname(resource.filename)
+        when ".mp3"; mp3s << resource.filename.chomp(".mp3")
+        when ".ogg"; oggs << resource.filename.chomp(".ogg")
+        else; others << resource.filename
       end
+    end
+    mp3s.each do |mp3|
+      html << display_mp3(mp3, oggs.include?(mp3))
+    end
+    oggs.each do |ogg|
+      html << display_ogg(ogg) unless mp3s.include? ogg
+    end
+    others.each do |other|
+      html << display_simple_file(resource.filename)
     end
     
     html << "</ul>"
